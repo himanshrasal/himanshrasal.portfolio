45368ac1535afa48290ddfe783a6a78abf90e6fc
himanshrasal <72780844+himanshrasal@users.noreply.github.com>
2023-10-18 06:53:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+const mode = document.querySelector('body');
+
+const cursorRounded = document.getElementById('cursor')
+cursorRounded.style.visibility = "hidden";
+
+const moveCursor = (e) => {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+  cursorRounded.style.visibility = 'visible';
+  cursorRounded.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+  console.log(mouseX, mouseY);
+
+}
+window.addEventListener('mousemove', moveCursor)
+
+
+//----------------------------------------------------------------------------------
+
+//button
+// const btn = document.querySelector('.switch');
+// var lightMode = false;
+// btn.addEventListener('click', (evt) => {
+//   if (!lightMode) {
+//     btn.classList.remove('toggleOff');
+//     btn.classList.add('toggleOn');
+//     lightMode = true;
+//   }
+//   else {
+//     btn.classList.remove('toggleOn');
+//     btn.classList.add('toggleOff');
+//     lightMode = false;
+//   }
+
+//   document.body.classList.toggle('darkMode');
+
+// }
+// );
+
+
+//--------------------------------------------------------------------------------
+
+const parallax_el = document.querySelectorAll('.parallax');
+let xValue = 0, yValue = 0;
+
+function update(cursorX, cursorY) {
+  xValue = cursorX - window.innerWidth / 2;
+  yValue = cursorY - window.innerHeight / 2;
+
+  parallax_el.forEach(el => {
+    let speedx = el.dataset.speedx;
+    let speedy = el.dataset.speedy;
+
+    el.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${-yValue * speedy}px))`;
+  })
+}
+
+update(0, 0);
+
+window.addEventListener("mousemove", (e) => {
+
+  update(e.clientX, e.clientY);
+})
+
+
+//-------------------------------------------------------------------------------
